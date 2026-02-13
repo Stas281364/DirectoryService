95cd0ba49052ae87ef66f3be65e67c5d6a574786
Stas <Stas200086@mail.ru>
2026-02-13 22:59:45 +0300

    Первая задача по созданию доменного проекта. Выполнил согласно пунктам в git`e. Также сохранилась проблема с StyleCop

---
+++ DirectoryService/src/DirectoryService.Domain/DepartmentPosition/DepartmentPosition.cs
@@ -0,0 +1,8 @@
+﻿namespace DirectoryService.Domain.DepartmentPosition;
+
+public class DepartmentPosition
+{
+    public Guid Id { get; set; }
+    public required List<Guid> DepartmentIds { get; set; }
+    public required List<Guid> PositionIds { get; set; }
+}
